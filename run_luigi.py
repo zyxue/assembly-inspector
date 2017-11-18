02bcbd228b5c8c48e709da44690711d0f954e2a3
Zhuyi Xue <alfred532008@gmail.com>
2017-11-17 16:26:37 -0800

    implemented FilterBam and FilterBams

--- run_luigi.py
+++ run_luigi.py
@@ -1,4 +1,5 @@
 import os
+import json
 import subprocess
 
 import numpy as np
@@ -7,9 +8,9 @@ import luigi
 
 
 class SelectLongestContigs(luigi.Task):
+    in_fai = luigi.Parameter()
     top_n = luigi.IntParameter(default=3)
     out_dir = luigi.Parameter()
-    in_fai = luigi.Parameter()
 
     def requires(self):
         return []
@@ -28,5 +29,52 @@ class SelectLongestContigs(luigi.Task):
         df_len.sort_values('len', ascending=False, inplace=True)
         df_len.head(self.top_n).to_csv(self.output().fn, index=False)
 
+
+class FilterBam(luigi.Task):
+    bam = luigi.Parameter()
+    in_fai = luigi.Parameter()
+    top_n = luigi.IntParameter(default=3)
+    out_dir = luigi.Parameter()
+
+    def requires(self):
+        return SelectLongestContigs(
+            in_fai=self.in_fai,
+            top_n=self.top_n,
+            out_dir=self.out_dir,
+        )
+
+    def output(self):
+        out_fn = '{0}.filtered.bam'.format(
+            os.path.basename(self.bam).rstrip('.bam'))
+        # out_path = os.path.join(self.out_dir, out_fn)
+        out_path = os.path.join(os.path.dirname(self.bam), out_fn)
+        print(out_path)
+        return luigi.LocalTarget(out_path)
+
+    def run(self):
+        contigs = ' '.join(
+            pd.read_csv(self.input().fn).ref_name.astype(str).values.tolist())
+
+        cmd = 'samtools view -hb {in_bam} {contigs} > {out_bam}'.format(
+            in_bam=self.bam,
+            contigs=contigs,
+            out_bam=self.output().fn)
+        print(cmd)
+        subprocess.call(cmd, shell=True, executable="/bin/bash")
+
+
+class FilterBams(luigi.WrapperTask):
+    bams = luigi.ListParameter()
+    in_fai = luigi.Parameter()
+    top_n = luigi.IntParameter(default=3)
+    out_dir = luigi.Parameter()
+
+    def requires(self):
+        for bam in self.bams:
+            yield FilterBam(
+                bam=bam, in_fai=self.in_fai,
+                top_n=self.top_n, out_dir=self.out_dir)
+
+                 
 if __name__ == '__main__':
     luigi.run()
