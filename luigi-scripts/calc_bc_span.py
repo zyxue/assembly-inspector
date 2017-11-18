6d0fb93097b4deb8a80385c6c68b3924c38503ab
Zhuyi Xue <alfred532008@gmail.com>
2017-11-17 19:18:06 -0800

    more renaming in calc_bc_span.py

--- luigi-scripts/calc_bc_span.py
+++ luigi-scripts/calc_bc_span.py
@@ -38,13 +38,14 @@ def gen_span_tuple(rec):
     )
 
 
-def pass_qc(sam_record):
-    rec = sam_record
-    return not (
-        rec.reference_id == -1
-        or rec.mapping_quality == 0
-        or rec.reference_end is None  # this could be a result of MAPQ=0
-    )
+# def pass_qc(sam_record):
+#     """pass quality check or not"""
+#     rec = sam_record
+#     return not (
+#         rec.reference_id == -1
+#         or rec.mapping_quality == 0
+#         or rec.reference_end is None  # this could be a result of MAPQ=0
+#     )
 
 
 # def get_lib_id(input_bam, dd):
@@ -53,27 +54,28 @@ def pass_qc(sam_record):
 #             return dd[k]
 
 
-def gen_cov_table(input_bams, lib_id=''):
+def gen_span_dd_and_read_cov(input_bams, lib_id=''):
     """lib_id: library id, used to disambiguate barcodes from different
     libraries"""
-    results = {}
+    span_dd = {}
+
     total_counts = 0
     for ibam in input_bams:
         logging.info('reading {0}'.format(ibam))
         infile = pysam.AlignmentFile(ibam)
 
         for k, rec in enumerate(infile):
-            if not pass_qc(rec):
-                continue
+            # if not pass_qc(rec):
+            #     continue
 
             ref_name = rec.reference_name
             bc = extract_barcode(rec.query_name, lib_id)
 
             span = gen_span_tuple(rec)
 
-            ref_dd = results.get(ref_name)
+            ref_dd = span_dd.get(ref_name)
             if ref_dd is None:
-                results[ref_name] = {bc: span}
+                span_dd[ref_name] = {bc: span}
             else:
                 old_tuple = ref_dd.get(bc)
                 if old_tuple is None:
@@ -91,16 +93,16 @@ def gen_cov_table(input_bams, lib_id=''):
     logging.info(
         'processed {0} records in total from {1} bam files'.format(
             total_counts, len(input_bams)))
-    return results
+    return span_dd
 
 
-def write_results(results, output):
+def write_span_results(span_dd, output):
     logging.info('writing results to {0}'.format(output))
 
     with open(output, 'wt') as opf:
         csvwriter = csv.writer(opf, delimiter='\t')
-        total_contigs = len(results.keys())
-        for ck, (contig, val_dd) in enumerate(results.items()):
+        total_contigs = len(span_dd.keys())
+        for ck, (contig, val_dd) in enumerate(span_dd.items()):
             for (bc, values) in val_dd.items():
                 if lib_id:
                     bc += '-{0}'.format(lib_id)
@@ -117,5 +119,5 @@ if __name__ == "__main__":
     lib_id = sys.argv[2]
     in_bams = list(sorted(sys.argv[3:]))  # e.g. from the same library
 
-    res = gen_cov_table(in_bams, lib_id)
-    write_results(res, out_csv)
+    res = gen_span_dd_and_read_cov(in_bams, lib_id)
+    write_span_results(res, out_csv)
