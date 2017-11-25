93ea7875bc47ebe95d61d3376e933e520862f774
Zhuyi Xue <alfred532008@gmail.com>
2017-11-25 09:50:59 -0800

    updated calcNumBreakPointsPTCs.scala

--- for-spark/calcNumBreakPointsPTCs.scala
+++ for-spark/calcNumBreakPointsPTCs.scala
@@ -1,3 +1,6 @@
+spark.conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+spark.conf.set("spark.executor.memory", "300g")
+
 import scala.collection.immutable.Map
 
 import org.apache.spark.sql.expressions.Aggregator
@@ -104,75 +107,27 @@ def time[R](block: => R): R = {
   val t0 = System.nanoTime()
   val result = block    // call-by-name
   val t1 = System.nanoTime()
-  println("Elapsed time: " + (t1 - t0) + "ns")
+  println("Elapsed time: " + (t1 - t0) / 1e9 + "s")
   result
 }
 
 // val ds = spark.read.option("sep", "\t").schema(spanSchema).csv("/projects/btl/zxue/assembly_correction/celegans/toy_cov.csv").as[Span]
 // val ds = spark.read.option("sep", "\t").schema(spanSchema).csv("/projects/btl/zxue/assembly_correction/celegans/cov.csv").as[Span]
-// val df = spark.read.csv("/projects/btl/zxue/assembly_correction/spruce/BX/agg_cov.csv/*.csv")
 val ds = spark.read.schema(spanSchema).csv("/projects/btl/zxue/assembly_correction/spruce/BX/agg_cov.concat.csv").as[Span]
 
 // println(ds.count())
 // println(ds.filter("read_count > 20").count())
 
 val cc = CalcBreakPoints.toColumn.name("bp")
-//.length >0 remove ref_names without break points detected
 
-val res = ds.filter("read_count > 20").groupByKey(a => a.ref_name).agg(cc)
+
+// val res = ds.groupByKey(a => a.ref_name).agg(cc)
+val res = ds.filter("read_count > 5").groupByKey(a => a.ref_name).agg(cc)
+res.persist
+
+//.length >0 remove ref_names without break points detected
 // reformatting into (ref_name, break_point) format for easier later processing
 val colNames = Seq("ref_name", "break_point")
-val out = res  .filter(_._2.length > 0).flatMap(i => i._2.map(j => (i._1, j))).toDF(colNames: _*)
-out.persist
-time {out.write.format("parquet").mode("overwrite").save("/projects/btl/zxue/assembly_correction/for-spark/lele.parquet")}
+val out = res.filter(_._2.length > 0).flatMap(i => i._2.map(j => (i._1, j))).toDF(colNames: _*)
 
-
-// // def initMap(beg: Int, end: Int): Map[Int, Int] = {
-// //   Map[Int, Int]() ++ (beg until end).map(i => (i -> 1))
-// // }
-
-
-// // def mergeMap(m1: Map[Int, Int], m2: Map[Int, Int]): Map[Int, Int] = {
-// //   m2.foreach {
-// //     case (key, value) => m1 += (key -> (value + m1.getOrElse[Int](key, 0)))
-// //   }
-// //   m1
-// // }
-
-
-// // def initCov(beg: Int, end: Int): IndexedSeq[(Int, Int)] = {
-// //   (beg until end).map(i => (i -> 1))
-// // }
-
-
-// // def mergeCov(m1: IndexedSeq[(Int, Int)], m2: IndexedSeq[(Int, Int)]): IndexedSeq[(Int, Int)] = {
-// //   (m1 ++ m2)
-// //     .groupBy(i => i._1)
-// //     .map(j => (j._1, j._2.map(k => k._2).sum))
-// //     .toIndexedSeq
-// //     // .sortBy(i => i._1)
-// // }
-
-
-// // val r = ds.map(span => (span.ref_name, initCov(span.beg, span.end)))
-
-// // val r2 = r.groupByKey(i => i._1)
-
-// // val r3 = r2.reduceGroups((a, b) => (a._1, mergeCov(a._2, b._2)))
-
-// // val r4 = r3.collect()
-
-
-// // val r =  ds.flatMap(span => Array.range(span.beg, span.end))
-// // val r2 = r.map(c => (c, 1))
-// // val r3 = r2.groupByKey(_._1).reduceGroups((a, b) => (a._1, (a._2 + b._2)).map(_._2)
-
-
-// val input =  sc.textFile("/projects/btl/zxue/assembly_correction/celegans/toy_cov.csv")
-// // val input =  sc.textFile("/projects/btl/zxue/assembly_correction/celegans/cov.csv")
-// // // Split up into words.
-// // val words = input.map(line => line.split("\t")).flatMap(ls => Array.range(ls(2).toInt, ls(3).toInt))
-// // // Transform into word and count.
-// // val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
-// // // Save the word count back out to a text file, causing evaluation.
-// // counts.saveAsTextFile("/projects/btl/zxue/assembly_correction/for-spark/lele")
+time {out.write.format("parquet").mode("overwrite").save("/projects/btl/zxue/assembly_correction/for-spark/filter-by-read-count-gt-5.parquet")}
